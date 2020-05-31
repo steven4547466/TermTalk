445bd51a6ea6b82212890ac3c78a8138c4cc94b5
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-05-31 19:09:08 -0400

    passwrd prompt

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 const socket = require('socket.io-client')('http://localhost:3000');
 const repl = require('repl')
-const replserv = new repl.REPLServer({
+const { prompt, Input } = require('enquirer')
+
+socket.on('disconnect', function() {
+	socket.emit('Disconnected from the server.')
+})
+
+socket.on('connect', async () => {
+	process.stdout.clearLine()
+	console.log("Connected to the server.")
+	const response = await prompt([
+	  {
+	    type: "input",
+	    name: "username",
+	    message: "What is your username?"
+	  },
+	  {
+	    type: "password",
+	    name: "pass",
+	    message: "What is your password?"
+	  }
+	]);
+ 
+	socket.emit("login", response)
+	socket.on("ready", () => {
+		new Input({
+		  type: "input",
+		  name: "msg",
+		  message: ""
+		}).run().then(msg => {
+			console.log(msg)
+			const username = response.username
+			socket.send({msg, username})
+		}).catch(err => {
+			console.error(err)
+			process.exit(1)
+		})
+	})
+})
+/*const replserv = new repl.REPLServer({
 	prompt: "-> ", 
 	eval: (input) => {
 		const username = "sammy"
 		socket.send({input, username})
 	}
-})
-socket.on('disconnect', function() {
-	socket.emit('disconnect')
-})
-
-socket.on('connect', () => {
-	process.stdout.clearLine()
-	console.log("Connected")
-	replserv.displayPrompt()
-
-})
-
+})*/
 socket.on('message', (data) => {
 	process.stdout.clearLine()
-	const { input, username } = data
+	const { message } = data
 	console.log(username + ': ' + input.split('\n')[0]);
 	replserv.displayPrompt()
 })
-
-replserv.on('exit', () => {
+/*replserv.on('exit', () => {
   process.exit();
-});
+});*/
 /*const ci = rl.createInterface({
 	input: process.stdin,
 	output: process.stdout
