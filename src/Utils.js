736bb3856afbd4e068e3f57eb4157520a9ab5e56
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-06-11 17:40:47 -0400

    fix duplicate ip list

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,21 @@
+/*
+	TermTalk - A simple and straightforward way of talking to your peers in the terminal.
+	Copyright (C) 2020 Terminalfreaks
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const fs = require("fs")
 const os = require("os")
 
@@ -24,8 +42,8 @@ class Utils {
 	}
 
 	static addToIps(ip) {
-		let config = Utils.config
-		if (config.ips.includes(ip)) return
+		let config = Utils.cachedConfig
+		if (config.ips.includes(ip)) return;
 		config.ips.push(ip)
 		this.overWriteConfig(config)
 	}
