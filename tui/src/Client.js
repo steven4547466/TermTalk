29ed2b8ce311647a532edc79c4fdc6f419376bf6
Steven Webster <steven@is-just-a.dev>
2020-06-12 11:25:17 -0400

    Auto stash before merge of "master" and "forked-origin/master"

--- tui/src/Client.js
+++ tui/src/Client.js
@@ -47,11 +47,12 @@ class ClientTUI {
 			label: "Messages",
 			tags: true,
 			style: {
-				fg: "green",
+				fg: "cyan",
 				border: {
-					fg: "cyan"
+					fg: "blue"
 				}
-			}
+			},
+			screen: screen
 		})
 
 		let messageBox = blessed.textarea({
@@ -67,7 +68,7 @@ class ClientTUI {
 			style: {
 				fg: Utils.config.chatColor,
 				border: {
-					fg: "cyan"
+					fg: "blue"
 				}
 			}
 		})
@@ -77,9 +78,9 @@ class ClientTUI {
 		form.on("submit", () => {
 			const msg = sanitize(messageBox.getValue())
 			messageBox.clearValue()
-			if (this._handleCommands(msg.trim(), messages, screen, messageBox)) return
+			if (this._handleCommands(msg.trim(), messages, screen)) return
 			socket.emit("msg", { msg, username: user.username, tag: user.tag, uid: user.uid, sessionID: user.sessionID })
-			messages.log(`${this.textPrefix}${user.username}#${user.tag} > ${msg}${this.textSuffix}`)
+			messages.log(`${this.textPrefix}${user.username}#${user.tag} > ${msg.trim()}${this.textSuffix}`, this.textPrefix, this.textSuffix)
 		})
 
 		socket.on('msg', (data) => {
@@ -89,24 +90,24 @@ class ClientTUI {
 		})
 
 		socket.on("disconnect", () => {
-			messages.log(`{red-fg}Client > You have been disconnected.{/red-fg}`)
+			messages.log(`{red-fg}Client > You have been disconnected.{/red-fg}`, this.textPrefix, this.textSuffix)
 		})
 
 		socket.on("reconnect", (attempt) => {
-			messages.log(`{red-fg}Client > Reconnected after ${attempt} attempt(s).{/red-fg}`)
+			messages.log(`{red-fg}Client > Reconnected after ${attempt} attempt(s).{/red-fg}`, this.textPrefix, this.textSuffix)
 		})
 
 		socket.on("reconnect_attempt", (attempt) => {
-			messages.log(`{red-fg}Client > Attempting reconnect. #${attempt}{/red-fg}`)
+			messages.log(`{red-fg}Client > Attempting reconnect. #${attempt}{/red-fg}`, this.textPrefix, this.textSuffix)
 		})
 
-		socket.on("getUserData", () => {
+		socket.on("get_user_data", () => {
 			socket.emit("return_user_data", user)
 		})
 
-		socket.on("methodResult", (data) => {
+		socket.on("method_result", (data) => {
 			if (!data.success) {
-				if (data.method == "messageSend") messages.log(`{red-fg}Client > ${data.message}{/red-fg}`)
+				if (data.method == "messageSend") messages.log(`{red-fg}Client > ${data.message.trim()}{/red-fg}`, this.textPrefix, this.textSuffix)
 			}
 		})
 
@@ -117,28 +118,22 @@ class ClientTUI {
 		screen.render()
 	}
 
-	static _handleCommands(message, messageLog, screen, ...handleArgs) {
+	static _handleCommands(message, messages, screen) {
 		if (!message.startsWith("/")) return false
-
 		const command = message.slice(1).split(" ")[0]
 		const args = message.slice(command.length + 1).trim().split(" ")
-
-		let colorRegex = /(#(\d|[a-f]){6})/i
+		let colorRegex = /(#(\d|[a-f]){6})-text/i
 		let matches;
-
 		if (matches = colorRegex.exec(message)) {
 			Utils.setMainTextColor(matches[1])
 			this.textPrefix = `{${matches[1]}-fg}`
 			this.textSuffix = `{/${matches[1]}-fg}`
-			handleArgs[0].style.fg = Utils.config.chatColor
-			messageLog.log(`${this.textPrefix}Messages are now the color ${matches[1]}.${this.textSuffix}`)
-
 			return true
 		} else if (command) {
 			switch (command) {
 				case "connect":
 					const newSocket = io(args[0].startsWith("http") ? args[0] : `http://${args[0]}`)
-					messageLog.log("Client > Connecting to different server...")
+					messages.log("Client > Connecting to different server...")
 					newSocket.on('connect', () => {
 						socket.disconnect()
 						socket.removeAllListeners()
@@ -155,7 +150,7 @@ class ClientTUI {
 }
 
 function sanitize(text) {
-	return text.replace(/\{/g, "{ ")
+	return blessed.escape(text)
 }
 
 module.exports = ClientTUI;
